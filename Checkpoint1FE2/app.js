6dee372599d8d61480cba981df5ee438f96782e4
viniTWL <vinigdc12@gmail.com>
2021-11-04 21:07:42 -0300

    novas funcionalidades
    condicionais no form, estrutura de repetição para pegar o valor do input radio

--- Checkpoint1FE2/app.js
+++ Checkpoint1FE2/app.js
@@ -1,52 +1,58 @@
+btn = document.querySelector('#btnSubmit');
 
-let title = document.querySelector('#nome-imagem')
-let desc = document.querySelector('#descricao')
-let image = document.querySelector('#url')
+    // Selecionando as variaves do HTML pelo ID
 
-btn = document.querySelector('#btnSubmit')
-
+let title = document.querySelector('#nome-imagem');
+let desc = document.querySelector('#descricao');
+let image = document.querySelector('#url');
 let cardGrid = document.querySelector('#card-grid');
 
+    // Evento do Botão, a cada clique criará uma nova div
+btn.addEventListener('click', (load) => {
+        
+        // Criando os elementos que irão compor a página
+
 let card = document.createElement('div');
-
 let cardTitle = document.createElement('h2');
-
 let divImg = document.createElement('div');
-
 let img = document.createElement('img');
-
 let cardParagraph = document.createElement('p');
+
+        // Pegando o valor do input radio que está marcado
+
+let options = [];
+
+let choice = document.getElementsByName('position');
+for (var i=0;i<choice.length;i++) {
+    if ( choice[i].checked ) {
+    options.push(choice[i].value);
+        }
+    };
+
+        // Colocando a classe card na Div Card
 
 card.setAttribute('class', 'card');
 
- btn.addEventListener('click', (load) => {
-    let title = document.querySelector('#nome-imagem')
-    let desc = document.querySelector('#descricao')
-    let image = document.querySelector('#url')
-    
-    btn = document.querySelector('#btnSubmit')
-    
-    let cardGrid = document.querySelector('#card-grid');
-    
-    let card = document.createElement('div');
-    
-    let cardTitle = document.createElement('h2');
-    
-    let divImg = document.createElement('div');
-    
-    let img = document.createElement('img');
-    
-    let cardParagraph = document.createElement('p');
-    
-    card.setAttribute('class', 'card');
-    
-    cardGrid.appendChild(card);
-    cardTitle.innerText = title.value;
-    divImg.appendChild(img);
-    img.setAttribute('src',image.value)
-    cardParagraph.innerText = desc.value;
-    card.appendChild(img);
-    card.appendChild(cardTitle);
-    card.appendChild(cardParagraph);
+        // Adiconando as informações do input nas varíaveis
+
+cardTitle.innerText = title.value + ` - ${options[0]}`;
+img.setAttribute('src',image.value);
+cardParagraph.innerText = desc.value;
+
+        // Condição caso um dos campos não estejam preenchidos
+
+if(title.value == "" || image.value == "" || desc.value == ""){
+        alert('Faltam campos a ser preenchidos!')
+        }
+else{
+        cardGrid.appendChild(card);
+        divImg.appendChild(img);
+        card.appendChild(divImg);
+        card.appendChild(cardTitle);
+        card.appendChild(cardParagraph);
+    }
+
+        // Não deixar que a página recarregue ao clicar no botão
+
     load.preventDefault();
 });
